725d79be1c766e1fdc704dad2183e3c7638f5b0a
Elias Rammos <eramm@192.168.1.7>
2023-05-15 23:23:46 +0300

    added basic cmake project structure

---
+++ include/fluffy/shader.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_SHADER_H_
+#define INCLUDE_SHADER_H_
+// Copyright 2023 Elias Rammos
+class Shader {
+};
+
+#endif  // INCLUDE_SHADER_H_
+
---
+++ src/main.cc
@@ -0,0 +1,7 @@
+// Copyright 2023 Elias Rammos
+#include <iostream>
+#include "fluffy/shader.h"
+
+int main() {
+        std::cout << "Hello World\n";
+}
