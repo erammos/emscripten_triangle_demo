027233652f7a757e9e2a2de72733fa42536752cf
Elias Rammos <eramm@192.168.1.7>
2023-05-17 00:35:16 +0300

    Added SDL support

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,58 @@
 // Copyright 2023 Elias Rammos
-#include <iostream>
-#include "fluffy/shader.h"
+#include <SDL2/SDL.h>
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
 
-int main() {
-        std::cout << "Hello World\n";
+#include <iostream>
+
+#include "fluffy/shader.h"
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Event event;
+
+void loop() {
+  if (SDL_PollEvent(&event)) {
+    switch (event.type) {
+      case SDL_QUIT:
+        std::cout << "wtf\n";
+        exit(0);
+        break;
+      default:
+        break;
+    }
+    SDL_SetRenderDrawColor(renderer, 96, 128, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+  }
+}
+
+int main(int argc, char **argv) {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cout << "Couldn't initialize SDL " << SDL_GetError();
+    exit(1);
+  }
+  window =
+      SDL_CreateWindow("Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                       SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+  if (!window) {
+    std::cout << "Failed to open window" << SDL_GetError();
+    exit(1);
+  }
+
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+  if (!renderer) {
+    std::cout << "Failed to create renderer " << SDL_GetError();
+    exit(1);
+  }
+#ifdef __EMSCRIPTEN__
+  emscripten_set_main_loop(loop , 0, true);
+#else
+while(true)
+    loop();
+#endif
 }
