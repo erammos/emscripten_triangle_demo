df8337aa31df726baef3f0ebfcdfedb014a5482d
Elias Rammos <eramm@192.168.1.7>
2023-05-18 01:20:50 +0300

    Added Opengl Support for hmtl5 and Macos

--- include/fluffy/shader.h
+++ include/fluffy/shader.h
@@ -1,6 +1,9 @@
 #ifndef INCLUDE_FLUFFY_SHADER_H_
 #define INCLUDE_FLUFFY_SHADER_H_
 // Copyright 2023 Elias Rammos
-class Shader {};
+class Shader {
+ public:
+ private:
+};
 
 #endif  // INCLUDE_FLUFFY_SHADER_H_
--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,8 @@
 // Copyright 2023 Elias Rammos
 #include <SDL2/SDL.h>
+
+#include "glad/glad.h"
+
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
@@ -10,12 +13,12 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-SDL_Window *window;
-SDL_Renderer *renderer;
+SDL_Window *window = nullptr;
 SDL_Event event;
+SDL_GLContext glContext = nullptr;
 
 void loop() {
-  if (SDL_PollEvent(&event)) {
+  while (SDL_PollEvent(&event)) {
     switch (event.type) {
       case SDL_QUIT:
         std::cout << "wtf\n";
@@ -24,9 +27,6 @@ void loop() {
       default:
         break;
     }
-    SDL_SetRenderDrawColor(renderer, 96, 128, 255, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
   }
 }
 
@@ -35,23 +35,36 @@ int main(int argc, char **argv) {
     std::cout << "Couldn't initialize SDL " << SDL_GetError();
     exit(1);
   }
+
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                      SDL_GL_CONTEXT_PROFILE_CORE | SDL_GL_CONTEXT_PROFILE_ES);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
   window =
       SDL_CreateWindow("Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                       SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+                       SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
   if (!window) {
     std::cout << "Failed to open window" << SDL_GetError();
     exit(1);
   }
 
-  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-  if (!renderer) {
-    std::cout << "Failed to create renderer " << SDL_GetError();
-    exit(1);
+  glContext = SDL_GL_CreateContext(window);
+  if (glContext == nullptr) {
+    std::cout << "Opengl Context error " << SDL_GetError();
+    exit(0);
   }
+
+  gladLoadGLLoader(SDL_GL_GetProcAddress);
+  std::cout << glGetString(GL_VENDOR) << "\n";
+  std::cout << glGetString(GL_RENDERER) << "\n";
+  std::cout << glGetString(GL_VERSION) << "\n";
 #ifdef __EMSCRIPTEN__
   emscripten_set_main_loop(loop, 0, true);
 #else
   while (true) loop();
 #endif
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 }
